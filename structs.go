a475a28e94697871974c3665421f3215a6257739
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2026-01-20 17:49:36 -0600

    structs in go

---
+++ structs.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type Person struct {
+    Name string
+    Age  int
+}
+type Address struct {
+    Street, City, State string
+	Pincode int
+}
+
+func main(){
+	person1 := Person{Name: "Alice", Age: 30}
+	fmt.Println(person1.Name, person1.Age)
+}
+
